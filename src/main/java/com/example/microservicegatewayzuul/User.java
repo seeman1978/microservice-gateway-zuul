40b315ba6a715870c49b97cba69c9544dadeed81
wangqiang <seeman1978@163.com>
2019-09-03 18:51:29 +0800

    Initial commit

---
+++ src/main/java/com/example/microservicegatewayzuul/User.java
@@ -0,0 +1,55 @@
+package com.example.microservicegatewayzuul;
+
+import java.math.BigDecimal;
+
+public class User
+{
+    public Long id;
+
+    public String username;
+
+    public String name;
+
+    public Integer age;
+
+    public BigDecimal balance;
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return this.age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public BigDecimal getBalance() {
+        return this.balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+}
